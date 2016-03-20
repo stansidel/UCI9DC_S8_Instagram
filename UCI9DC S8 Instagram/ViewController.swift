4ae1f58c0c81929aed904cedf346b978f35b424f
Stanislav Sidelnikov <sidelnikov.stanislav@gmail.com>
2016-03-20 11:52:33 +0500

    Initial Commit

---
+++ UCI9DC S8 Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UCI9DC S8 Instagram
+//
+//  Created by Stanislav Sidelnikov on 20/03/16.
+//  Copyright © 2016 Stanislav Sidelnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
